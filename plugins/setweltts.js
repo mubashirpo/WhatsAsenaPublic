7683d328c3dc2caa82ba70359964456be43c79cd
T0X!C D3V!L <78701752+TOXIC-DEVIL@users.noreply.github.com>
2021-04-09 23:41:08 +0530

    Update setweltts.js

--- plugins/setweltts.js
+++ plugins/setweltts.js
@@ -13,7 +13,7 @@ const fs = require('fs');
 const https = require('https');
 const googleTTS = require('google-translate-tts');
 
-Asena.addCommand({pattern: 'setweltts', fromMe: false}, (async (message, match) => {
+Asena.addCommand({pattern: 'setweltts', fromMe: true}, (async (message, match) => {
 
     await message.sendMessage("To Set Welcome Message To Voice, Please Type :   *.welcome .weltts Type Your Welcome Voice*\n\nExample :   *.welcome .weltts Hey There I Am Using WhatsAsenaPublic*\n\nYou Can Also Use Different Languages For Welcome Voice. You Can Use:\n\nAzerice => az, English => en, Türkçe => tr, Malayalam => ml, Hindi => hi, Indonesia => id, Español => es, Portuguese => pt\n\n.welcome .weltts Type Your Welcome Message {your language}\n\nExample By Using Different Languages:\n.welcome .weltts Hey Bro!! I am WhatsAsenaPublic {az}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {en}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {en}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {hi}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {ml}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {es}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {id}\n\n*NOTE: If You Didn't Add Any Language To The Welcome Voice, It Will Be the Default language of the bot & use this command in group*\n\nThank You For Choosing WhatsAsenaPublic 💌");
 
