91558192e8019ed2ff6ae6d49eee1ceb002ce861
T0X!C D3V!L <78701752+TOXIC-DEVIL@users.noreply.github.com>
2021-04-09 23:49:37 +0530

    Update setweltts.js

--- plugins/setweltts.js
+++ plugins/setweltts.js
@@ -13,12 +13,6 @@ const fs = require('fs');
 const https = require('https');
 const googleTTS = require('google-translate-tts');
 
-Asena.addCommand({pattern: 'setweltts', fromMe: true}, (async (message, match) => {
-
-    await message.sendMessage("To Set Welcome Message To Voice, Please Type :   *.welcome .weltts Type Your Welcome Voice*\n\nExample :   *.welcome .weltts Hey There I Am Using WhatsAsenaPublic*\n\nYou Can Also Use Different Languages For Welcome Voice. You Can Use:\n\nAzerice => az, English => en, Türkçe => tr, Malayalam => ml, Hindi => hi, Indonesia => id, Español => es, Portuguese => pt\n\n.welcome .weltts Type Your Welcome Message {your language}\n\nExample By Using Different Languages:\n.welcome .weltts Hey Bro!! I am WhatsAsenaPublic {az}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {en}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {en}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {hi}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {ml}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {es}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {id}\n\n*NOTE: If You Didn't Add Any Language To The Welcome Voice, It Will Be the Default language of the bot & use this command in group*\n\nThank You For Choosing WhatsAsenaPublic 💌");
-
-}));
-
 Asena.addCommand({pattern: 'weltts (.*)', fromMe: true, dontAddCommandList: true }, (async (message, match) => {
     if(match[1] === undefined || match[1] == "")
         return;
@@ -42,4 +36,21 @@ Asena.addCommand({pattern: 'weltts (.*)', fromMe: true, dontAddCommandList: true
         voice: LANG
     });
     await message.client.sendMessage(message.jid,buffer, MessageType.audio, {mimetype: Mimetype.mp4Audio, ptt: true});
+}));
+
+if (Config.WORKTYPE == 'private') {
+
+    Asena.addCommand({pattern: 'setweltts', fromMe: true}, (async (message, match) => {
+
+    await message.sendMessage("To Set Welcome Message To Voice, Please Type :   *.welcome .weltts Type Your Welcome Voice*\n\nExample :   *.welcome .weltts Hey There I Am Using WhatsAsenaPublic*\n\nYou Can Also Use Different Languages For Welcome Voice. You Can Use:\n\nAzerice => az, English => en, Türkçe => tr, Malayalam => ml, Hindi => hi, Indonesia => id, Español => es, Portuguese => pt\n\n.welcome .weltts Type Your Welcome Message {your language}\n\nExample By Using Different Languages:\n.welcome .weltts Hey Bro!! I am WhatsAsenaPublic {az}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {en}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {en}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {hi}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {ml}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {es}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {id}\n\n*NOTE: If You Didn't Add Any Language To The Welcome Voice, It Will Be the Default language of the bot & use this command in group*\n\nThank You For Choosing WhatsAsenaPublic 💌");
+
+}));
+
+}
+else if (Config.WORKTYPE == 'public') {
+
+    Asena.addCommand({pattern: 'setweltts', fromMe: false}, (async (message, match) => {
+
+    await message.sendMessage("To Set Welcome Message To Voice, Please Type :   *.welcome .weltts Type Your Welcome Voice*\n\nExample :   *.welcome .weltts Hey There I Am Using WhatsAsenaPublic*\n\nYou Can Also Use Different Languages For Welcome Voice. You Can Use:\n\nAzerice => az, English => en, Türkçe => tr, Malayalam => ml, Hindi => hi, Indonesia => id, Español => es, Portuguese => pt\n\n.welcome .weltts Type Your Welcome Message {your language}\n\nExample By Using Different Languages:\n.welcome .weltts Hey Bro!! I am WhatsAsenaPublic {az}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {en}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {en}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {hi}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {ml}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {es}\nwelcome .weltts Hey Bro!! I am WhatsAsenaPublic {id}\n\n*NOTE: If You Didn't Add Any Language To The Welcome Voice, It Will Be the Default language of the bot & use this command in group*\n\nThank You For Choosing WhatsAsenaPublic 💌");
+
 }));
